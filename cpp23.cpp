7f676c7dacb8341342c97cd1e20eee59714861c3
realth000 <a598919335@gmail.com>
2024-04-18 20:49:52 +0800

    Add expected example

--- cpp23.cpp
+++ cpp23.cpp
@@ -1,6 +1,6 @@
-#include <optional>
 #include <format>
 #include <iostream>
+#include <optional>
 
 template <typename... _Args>
 void print(std::format_string<_Args...> fmt, _Args&&... __args) {
@@ -38,6 +38,36 @@ class Foo {
     Foo(double foo, int bar) : foo_(foo), bar_(bar){};
 };
 
+enum class BuildError {
+    invalid_args,
+    internl_error,
+};
+
+class MyExpect {
+   public:
+    double foo_;
+    int bar_;
+
+    static std::expected<MyExpect, BuildError> New(double foo, int bar) {
+        if (bar < 0) {
+            return std::unexpected(BuildError::invalid_args);
+        } else {
+            return MyExpect{foo, bar};
+        }
+    }
+
+    static MyExpect Default() { return MyExpect{0, 0}; }
+
+    void print_data() const { println("hello: foo_={}, bar_={}", foo_, bar_); }
+
+    void updateFoo(double foo) { foo_ = foo; }
+
+    void updateBar(double bar) { bar_ = bar; }
+
+   private:
+    MyExpect(double foo, int bar) : foo_(foo), bar_(bar) {}
+};
+
 std::optional<Foo> update(std::optional<Foo> foo) {
     foo->updateFoo(12.3);
     return std::optional(*foo);
@@ -48,13 +78,29 @@ Foo update_bar(std::optional<Foo> foo, int value) {
     return *foo;
 }
 
+std::expected<MyExpect, BuildError> update2(std::expected<MyExpect, BuildError> myExpect) {
+    myExpect->updateFoo(12.3);
+    return {*myExpect};
+}
+
+MyExpect update_bar2(std::expected<MyExpect, BuildError> myExpect, int bar) {
+    myExpect->updateBar(bar);
+    return *myExpect;
+}
+
 int main() {
-    auto foo = Foo::New(100, 200).and_then(update).transform([](foo x) { return update_bar(x, 345);}).or_else([] {
+    auto foo = Foo::New(100, 200).and_then(update).transform([](foo x) { return update_bar(x, 345); }).or_else([] {
         return std::optional<Foo>(Foo::Default());
     });
+
+    auto myExpect =
+        MyExpect::New(100, 200)
+            .and_then(update2)
+            .transform([](MyExpect x) { return update_bar2(x, 345); })
+            .or_else([](BuildError buildError) { return std::expected<MyExpect, BuildError>(MyExpect::Default()); });
+
+    myExpect->print_data();
 
     foo.hello();
     return 0;
 }
-
-
